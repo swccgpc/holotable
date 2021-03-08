677fb845d9744aa9c8d5f29ef8dfef345dfe9f63
Devon Hubner <devon@hubner.org>
2021-03-07 20:33:00 -0500

    info about missing cards

---
+++ case_fixer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import time
+
+print("\nFinding files \n")
+os.chdir("Images-HT/")
+fils = os.popen('find -type f').read()
+for fil in fils.split("\n"):
+  fil_pieces = fil.split("/")
+  filename   = fil_pieces[len(fil_pieces)-1]
+  filename_l = filename.lower()
+  if ("x"+fil != "x"):
+    if ((filename != filename_l) and
+        ("-title" not in filename) and
+        ("CardBack.gif" not in filename)
+       ):
+      print("  * ["+filename+"]: renaming to ["+filename_l+"]")
+      to = fil.replace(filename, filename_l)
+      dewit = "git mv "+fil+" "+to
+      print("    "+dewit)
+      os.popen(dewit)
+      time.sleep(2)
+
